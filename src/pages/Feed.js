7a45587657e0b50596ebbac95c86e0428b5bff26
Marcos Paulo <marcos.souza.anl@gmail.com>
2019-06-14 01:39:56 -0300

    estilizando a pagina de feed. As informações agora está vindo da base, feito na parte 2 dessa app, backend

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,70 +1,58 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+
+import './Feed.css';
+
 import more from '../assets/more.svg';
 import comment from '../assets/comment.svg';
 import like from '../assets/like.svg';
 import send from '../assets/send.svg';
 
-import './Feed.css';
-
 class Feed extends Component {
-    render() {
-        return (
-            <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Marcos Paulo</span>
-                            <span className="place">Pernambuco</span>
-                        </div>
+  state = {
+    feed: [],
+  };
 
-                        <img src={more} alt="Mais" />
-                    </header>
+  async componentDidMount() {
+    const response = await api.get('posts');
 
-                    <img src="http://localhost:3333/files/OmniStack.jpg"/>
+    this.setState({ feed: response.data });
+  };
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like}/>
-                            <img src={comment}/>
-                            <img src={send}/>
-                        </div>
+  render() {
+    return (
+      <section id="post-list">
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-                        <strong>900 curtidas</strong>
-                        <p>
-                            Semana muito bacana com o Diego da Rocketseat
-                            <span>#react #onmistack #node</span>
-                        </p>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Marcos Paulo</span>
-                            <span className="place">Pernambuco</span>
-                        </div>
+              <img src={more} alt="Mais" />
+            </header>
 
-                        <img src={more} alt="Mais" />
-                    </header>
+            <img src={`http://localhost:3333/files/${post.image}`} />
 
-                    <img src="http://localhost:3333/files/OmniStack.jpg"/>
+            <footer>
+              <div className="actions">
+                <img src={like} />
+                <img src={comment} />
+                <img src={send} />
+              </div>
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like}/>
-                            <img src={comment}/>
-                            <img src={send}/>
-                        </div>
-
-                        <strong>900 curtidas</strong>
-                        <p>
-                            Semana muito bacana com o Diego da Rocketseat
-                            <span>#react #onmistack #node</span>
-                        </p>
-                    </footer>
-                </article>
-            </section>
-        )
-    }
+              <strong>{post.likes}</strong>
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
+      </section>
+    )
+  }
 }
 
 export default Feed;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import './New.css'
 
 class New extends Component {
-    render() {
-        return (
-            <h1>New</h1>
-        )
-    }
+  render() {
+    return (
+      <form id="new-post">
+        <input type="file" />
+
+        <input
+          type="text"
+          name="author"
+          placeholder="Autor do post"
+        />
+        <input
+          type="text"
+          name="place"
+          placeholder="Local do post"
+        />
+
+        <input
+          type="text"
+          name="description"
+          placeholder="Descrição do post"
+        />
+
+        <input
+          type="text"
+          name="hashtags"
+          placeholder="Hashtags do post"
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
 }
 
 export default New;
